e9c8273767e6d8e3418c07df46025b049db7504b
harunisiaho <harunisiaho@gmail.com>
2024-04-13 19:31:01 +0300

    Added Jenkins file

--- app.py
+++ app.py
@@ -5,12 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # host_name = socket.gethostname()
-    # host_ip = socket.gethostbyname(host_name)
     host = request.headers.get('Host')
-    # return f'<p>Hello World from {host_name} with IP address {host_ip}! from {host}</p>'
-    return f'<p>Hello World. Your IP address is  {host.split(':')[0]}</p>'
-
+    return f'<p>Hello World. My IP address is  {host.split(':')[0]}</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
